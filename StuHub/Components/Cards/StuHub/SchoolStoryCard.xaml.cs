5794cbe75a97ced0bdd54c04c5990383642d500d
Strypper Vandel Jason <ocgrb.strypperjason115@gmail.com>
2020-09-05 09:28:57 +0700

    Version 1.0.0

---
+++ StuHub/Components/Cards/StuHub/SchoolStoryCard.xaml.cs
@@ -0,0 +1,90 @@
+﻿using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+
+// The User Control item template is documented at https://go.microsoft.com/fwlink/?LinkId=234236
+
+namespace StuHub.Components.Cards.StuHub
+{
+    public sealed partial class SchoolStoryCard : UserControl
+    {
+        public string StudentName
+        {
+            get { return (string)GetValue(PersonNameProperty); }
+            set { SetValue(PersonNameProperty, value); }
+        }
+
+        public static readonly DependencyProperty PersonNameProperty =
+            DependencyProperty.Register("StudentName", typeof(string), typeof(SchoolStoryCard), null);
+
+        public string SchoolName
+        {
+            get { return (string)GetValue(SchoolNameProperty); }
+            set { SetValue(SchoolNameProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SchoolName.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SchoolNameProperty =
+            DependencyProperty.Register("SchoolName", typeof(string), typeof(SchoolStoryCard), null);
+
+        public string ProfileUrl
+        {
+            get { return (string)GetValue(ProfileUrlProperty); }
+            set { SetValue(ProfileUrlProperty, value); }
+        }
+
+        public static readonly DependencyProperty ProfileUrlProperty =
+            DependencyProperty.Register("ProfileUrl", typeof(string), typeof(SchoolStoryCard), null);
+
+        public string SchoolUrl
+        {
+            get { return (string)GetValue(SchoolUrlProperty); }
+            set { SetValue(SchoolUrlProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for SchoolUrl.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty SchoolUrlProperty =
+            DependencyProperty.Register("SchoolUrl", typeof(string), typeof(SchoolStoryCard), null);
+
+        public string StoryImage
+        {
+            get { return (string)GetValue(StoryImageProperty); }
+            set { SetValue(StoryImageProperty, value); }
+        }
+
+        public static readonly DependencyProperty StoryImageProperty =
+            DependencyProperty.Register("StoryImage", typeof(string), typeof(SchoolStoryCard), null);
+
+        public string Story
+        {
+            get { return (string)GetValue(StoryProperty); }
+            set { SetValue(StoryProperty, value); }
+        }
+
+        public static readonly DependencyProperty StoryProperty =
+            DependencyProperty.Register("Story", typeof(string), typeof(SchoolStoryCard), null);
+
+        public SchoolStoryCard()
+        {
+            this.InitializeComponent();
+        }
+
+        private void CommentButton_Click(object sender, RoutedEventArgs e)
+        {
+            ToggleButton tB = sender as ToggleButton;
+            if (tB != null)
+            {
+                if (tB.IsChecked == true)
+                {
+                    tB.IsChecked = true;
+                    CommentSection.Visibility = Visibility.Visible;
+                }
+                else
+                {
+                    tB.IsChecked = false;
+                    CommentSection.Visibility = Visibility.Collapsed;
+                }
+            }
+        }
+    }
+}
---
+++ StuHub/Models/Stuhub/SchoolStory.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StuHub.Models.Stuhub
+{
+    public class SchoolStory
+    {
+        public int SchoolStoryId { get; set; }
+        public string SchoolId { get; set; }
+        public string StudentId { get; set; }
+        public string StudentImageProfileUrl { get; set; }
+        public string StudentName { get; set; }
+        public string SchoolName { get; set; }
+        public DateTime UploadDate { get; set; }
+        public string Story { get; set; }
+        public string Feeling { get; set; }
+        public string StoryImageUrl { get; set; }
+        public int Like { get; set; }
+    }
+}
